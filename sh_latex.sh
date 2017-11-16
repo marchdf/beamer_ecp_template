49e21fad7794b1e690c8a61c7f6c055e2000fb1f
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2017-11-16 09:15:59 -0700

    Added alternate title page.

--- sh_latex.sh
+++
@@ -1,7 +0,0 @@
-rm -f *.aux *.nav *.log *.out *.snm *.blg *.bbl *.toc *.vrb;
-pdflatex --shell-escape report;
-pdflatex --shell-escape report;
-bibtex report ;
-pdflatex --shell-escape report;
-pdflatex --shell-escape report;
-rm -f *.aux *.nav *.log *.out *.snm *.blg *.bbl *.toc *.vrb;
